aa202976403187ceac33d9d39291966f99d8338f
fabiannabi <fabiannabi@yahoo.com.mx>
2022-04-19 17:52:57 -0500

    Added first Cypress test for payment flow

---
+++ cypress/integration/payment_spec.js
@@ -0,0 +1,54 @@
+const {v4: uuidv4} = require('uuid')
+describe('payment', () => {
+  it('user can make payment', () => {
+
+    //login user
+    cy.visit('/');
+    cy.findByRole('textbox', {name: /username/i}).type('johndoe');
+    cy.findByLabelText(/password/i).type('s3cret');
+    cy.findByRole('checkbox', {  name: /remember me/i}).click();
+    cy.findByRole('button', {name: /sign in/i}).click();
+
+    //check account balance
+    let oldBalance
+    cy.get('[data-test="sidenav-user-balance"]').then(($balance) => oldBalance = $balance.text())
+
+    //click new buttons
+    cy.findByText(/new/i).click();
+
+    //look for user
+    cy.findByRole('textbox').type('devon becker');
+    cy.findByText(/devon becker/i).click();
+
+    //enter amounnt and notes
+    const payment = '100.00'
+    cy.findByPlaceholderText(/amount/i).type(payment)
+    const note = uuidv4();
+    cy.findByPlaceholderText(/add a note/i).type(note)
+
+
+    //click pay
+    cy.findByRole('button', {  name: /pay/i}).click();
+
+    //return to transactions
+    cy.findByRole('button', {  name: /return to transactions/i}).click();
+
+    //go to personal payents
+    cy.findByText(/mine/i).click();
+
+    //click on payment
+    cy.findByText(note).click({force:true});
+
+    //verify if payment was marked
+    cy.findByText(`-$${payment}`).should('be.visible')
+    cy.findByText(note).should('be.visible')
+
+    //verify amount is correct
+
+    cy.get('[data-test="sidenav-user-balance"]').then(($balance) => {
+      const oldAmount =  parseFloat(oldBalance.replace(/\$|,/g, ""));
+      const TransactionBalance = parseFloat($balance.text().replace(/\$|,/g, ""));
+      expect(oldAmount -TransactionBalance ).to.eq(parseFloat(payment))
+    })
+  })
+})
